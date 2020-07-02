a3d3032c59751acc927b5500ba156977d8c7b2ce
susanfochesatto <29303313+susanmiss@users.noreply.github.com>
2020-07-02 09:54:20 +0100

    Fix styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,14 +6,17 @@ function About() {
   return (
     <div className="container">
        <div className="about">
-            <p id="text-welcome">Hello and welcome to my website :)</p>
-            <h1 id="text-title">I’m a Software Engineer based in London - UK.</h1>
-            <p id="text-about">Prior to deciding I wanted to pursue coding as a career, I spent the last ten years leading financial and maintenance teams for a hospital group in Brazil. During this time, I gained a lot of professional experience which I have no doubt will help me to become a successful software engineer. I have always been curious about development and decided to take some additional courses to further my understanding and I haven’t looked back since. I am highly determined and extremely committed to pursuing a career in development.</p>
+       <p id="text-welcome">Hello, my name is Susan Missaglia. Welcome :)</p>
+            <h1 id="text-title">I’m a Junior Software Engineer based in London - UK.</h1>
+            <p id="text-about">Prior to deciding I wanted to pursue coding as a career, I spent the last ten years leading financial and maintenance teams for a hospital group in Brazil. During this time, I gained a lot of professional experience which I have no doubt will help me to become a successful software engineer.</p>
+            <p id="text-about">I applied to study at Futureproof Botcamp, and among more than 700 applicants, I've been selected. The bootcamp covered full stack development and gave me the skills needed to start a career as Junior Developer. </p>
+            <p id="text-about"> Version Control via Github, problem solving, clean code, TDD always as a goal. These are skills that I'm fully committed to achieve always. </p>
+            <p>MERN Technology is not just a work's tool, it's hobby and I love it.</p>
         </div>
 
         <div className="buttons-about">
-            <button id="btn-cv">Download CV</button>
-            <button id="btn-get-in-touch">Get in Touch</button>
+        <a href="https://media-exp1.licdn.com/dms/document/C512DAQEuGxY1YMLFiw/profile-treasury-document-pdf-analyzed/0?e=1593687600&v=beta&t=JIJ3Su_8FTikbHOf1gNvTxPnVNrNaHT56YwnZLYkuYA" target="_blank"> <button id="btn-cv">Download CV</button></a>
+            <a href="mailto: 'fsusan@icloud.com'"><button id="btn-get-in-touch">Get in Touch</button></a>
         </div>
 
     </div>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import "../style/Projects.css";
 import BloodDonationCover from "../images/bloodDonationCover.png";
 import BloodDonationCode from "../images/bloodDonationCode.png";
@@ -9,26 +9,37 @@ import QuarantineCode from '../images/quarantineCode.png';
 import ZombieCover from '../images/zombieCover.png'
 import ZombieCode from '../images/zombieCode.png'
 
-function Projects() {
+class Projects extends Component {
+
+
+  render(){
+   
+
   return (
     <div className="projects">
       <div className="row pg-1">
-        <div className="project-img column">
-          <img src={BloodDonationCover} alt="blood donation web site cover" />
-          {/* <img src={BloodDonationCode} alt="blood donation web site cover" /> */}
+
+       <div className="cf">
+          <img className="bottom" id="bdb" src={BloodDonationCode}/>
+          <img className="top" id="bdt" src={BloodDonationCover}/>
         </div>
 
         <div className="project-content column">
           <h2>Blood Donation</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            placerat turpis ac nisl ultricies mollis. Quisque blandit lobortis
-            bibendum. Morbi aliquam malesuada lorem, ac placerat quam malesuada
-            at. Proin fringilla dolor mauris, tincidunt gravida diam semper
-            quis. Praesent molestie id orci eget elementum. Vestibulum sed massa
-            tortor. Proin pharetra varius ultrices. Donec interdum eros et
-            luctus volutpat. Phasellus metus sem, feugiat nec gravida vel,
-            tempor eu tortor.
+          Blood Donation Finder is a project about people who want donate blood and Hospital/Clinics in need of it. The Hospitals can send a post to the website and the users can interage informing the hospital if they are going, send emails, and in their own dashboard can see last and next time to donate. 
+          <br />
+          <p>Technologies Used:</p>
+          <ul>
+            <li>MongoDB</li>
+            <li>Express</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Nodemailer</li>
+            <li>Bootstrap</li>
+            <li>AppointmentApp</li>
+
+          </ul>
           </p>
           <a href="https://github.com/susanmiss/blood-donation-finder" target="_blank"><button className="btn-github">Access on Github</button></a>
           
@@ -40,39 +51,45 @@ function Projects() {
         <div className="project-content column">
           <h2>Trip4Me</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            placerat turpis ac nisl ultricies mollis. Quisque blandit lobortis
-            bibendum. Morbi aliquam malesuada lorem, ac placerat quam malesuada
-            at. Proin fringilla dolor mauris, tincidunt gravida diam semper
-            quis. Praesent molestie id orci eget elementum. Vestibulum sed massa
-            tortor. Proin pharetra varius ultrices. Donec interdum eros et
-            luctus volutpat. Phasellus metus sem, feugiat nec gravida vel,
-            tempor eu tortor.
+          Trip4me is a project about my own trips. I wanted to create a website from scratch using MERN technology. There is an admin area where is possible to create, delete, and update a post with images.
+          <br />
+          <p>Technologies Used:</p>
+          <ul>
+            <li>MongoDB</li>
+            <li>Express</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Bootstrap</li>
+          </ul>
           </p>
           <a href="https://github.com/susanmiss/trip4me" target="_blank"><button className="btn-github">Access on Github</button></a>
         </div>
 
-        <div className="project-img column">
-          <img src={Trip4meCover} alt="blood donation web site cover" />
+       
+        <div className="cf">
+          <img className="bottom" src={Trip4meCode}/>
+          <img className="top" src={Trip4meCover}/>
         </div>
+    
       </div>
 
       <div className="row">
-        <div className="project-img column">
-          <img src={ZombieCover} alt="blood donation web site cover" />
+      <div className="cf">
+          <img className="bottom" src={ZombieCode}/>
+          <img className="top" src={ZombieCover}/>
         </div>
 
         <div className="project-content column">
           <h2>Zombie Movies</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            placerat turpis ac nisl ultricies mollis. Quisque blandit lobortis
-            bibendum. Morbi aliquam malesuada lorem, ac placerat quam malesuada
-            at. Proin fringilla dolor mauris, tincidunt gravida diam semper
-            quis. Praesent molestie id orci eget elementum. Vestibulum sed massa
-            tortor. Proin pharetra varius ultrices. Donec interdum eros et
-            luctus volutpat. Phasellus metus sem, feugiat nec gravida vel,
-            tempor eu tortor.
+          Zombie Movies is a project when you can see a list of zombie movies (my favorites ). There is a search bar when you can search by actor's name or movie's name - don't need do be zombies related. I created that project in order to learn more about fetching APis, express and requests.
+          <br />
+          <p>Technologies Used:</p>
+          <ul>
+            <li>Express</li>
+            <li>EJS - as engine template</li>
+            <li>External API integration</li>
+          </ul>
           </p>
           <a href="https://github.com/susanmiss/zombie-movies" target="_blank"><button className="btn-github">Access on Github</button></a>
         </div>
@@ -82,24 +99,28 @@ function Projects() {
         <div className="project-content column">
           <h2>Quarantine Thougts</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            placerat turpis ac nisl ultricies mollis. Quisque blandit lobortis
-            bibendum. Morbi aliquam malesuada lorem, ac placerat quam malesuada
-            at. Proin fringilla dolor mauris, tincidunt gravida diam semper
-            quis. Praesent molestie id orci eget elementum. Vestibulum sed massa
-            tortor. Proin pharetra varius ultrices. Donec interdum eros et
-            luctus volutpat. Phasellus metus sem, feugiat nec gravida vel,
-            tempor eu tortor.
+          Quarantine Thoughts is a simple blog when we can share our thoughts in this quarantine times. I wanted to create a blog from scratch using MERN technology. There is an admin area where is possible to create, delete, and update a post.
+          <br />
+          <p>Technologies Used:</p>
+          <ul>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>MongoDB</li>
+            <li>Node.js</li>
+            <li>Bootstrap</li>
+          </ul>
           </p>
           <a href="https://github.com/susanmiss/quarantine-thoughts" target="_blank"><button className="btn-github">Access on Github</button></a>
         </div>
 
-        <div className="project-img column">
-          <img src={QuarantineCover} alt="blood donation web site cover" />
+        <div className="cf">
+          <img className="bottom" src={QuarantineCode}/>
+          <img className="top" src={QuarantineCover}/>
         </div>
       </div>
     </div>
   );
+ }
 }
 
 export default Projects;
